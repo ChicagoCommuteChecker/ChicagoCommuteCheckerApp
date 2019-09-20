5a9344586434dc76f2af5554086459a79999df79
Alojzy Klim <adklim@gmail.com>
2019-09-20 14:14:57 -0500

    got query working for CTA lines

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,14 @@
-function redlineCheck() {
-  var queryURL =  "https://www.transitchicago.com/api/1.0/routes.aspx?routeid=red&outputType=JSON"
+$(".button").on("click", function() {
+
+    var queryURL =  "https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +$(this).attr("id") +"&outputType=JSON"
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
       console.log(response);
     })
-  }
+  })
 
-  redlineCheck();
+  
+
+
