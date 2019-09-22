0b639064c2b1feb63c665b4644952e4e6d95a502
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-22 15:34:28 -0500

    Think I've got the current weather api call test set up. Need to test it with the input form.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   $(".button").on("click", function() {
 
-      var queryURL = "https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +$(this).attr("id") +"&outputType=JSON"
+      var queryURL = "https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +$(this).attr("id") +"&outputType=JSON";
       $.ajax({
         url: queryURL,
         method: "GET"
@@ -12,11 +12,27 @@ $(document).ready(function(){
     })
 
   // Note. We'll need to make the selector here match the html once it gets added.
-  // $("#submitButton").on("clickl", function(){
+  $("#submitButton").on("clickl", function(){
     
-  //   var weatherQueryURL = "api.openweathermap.org/data/2.5/weather?zip=94040,us"
+    // var weatherQueryURL = "api.openweathermap.org/data/2.5/weather?zip=" + $("#inputFormID").val() + ",us&APIID=4df0823f371a314fac922d45729a9495";
+    var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=60091,us&apiid=4df0823f371a314fac922d45729a9495";
+    $.ajax({
+      url: weatherQueryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    })
+  })
 
-  // })
+  function weatherTest() {
+    var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=60091,us&apiid=4df0823f371a314fac922d45729a9495";
+    $.ajax({
+      url: weatherQueryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    })
+  }
 
-
+  weatherTest();
 })
