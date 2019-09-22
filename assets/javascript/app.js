99871d4727266454476cf451846468bdab7f7c84
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-22 15:10:06 -0500

    Put our javascript inside of a document ready function so it won't fire until the page is loaded. Started on some weather api calls that are currently commented out.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,22 @@
-$(".button").on("click", function() {
+$(document).ready(function(){
 
-    var queryURL =  "https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +$(this).attr("id") +"&outputType=JSON"
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
+  $(".button").on("click", function() {
+
+      var queryURL = "https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +$(this).attr("id") +"&outputType=JSON"
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      })
     })
+
+  // Note. We'll need to make the selector here match the html once it gets added.
+  $("#submitButton").on("clickl", function(){
+    
+    var weatherQueryURL = "api.openweathermap.org/data/2.5/weather?zip=94040,us"
+
   })
 
-  
 
-
+})
