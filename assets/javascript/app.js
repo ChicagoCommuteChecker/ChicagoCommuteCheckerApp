6be73182338cb60a78818eb7332d9ca966ac6181
Jordan Ricarte <jordan@ricarte.net>
2019-09-23 09:35:04 -0500

    test branch

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+var movie = "Mr. Nobody";
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        $("#title1").html(response.Title)
