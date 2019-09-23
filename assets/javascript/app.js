da92bd35e7ff58d847333de07ce37de9bc1a3abc
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-22 19:29:13 -0500

    Weather api calls (#14)
    * Put our javascript inside of a document ready function so it won't fire until the page is loaded. Started on some weather api calls that are currently commented out.
    
    * forgot to hit save before committing last time.
    
    * Think I've got the current weather api call test set up. Need to test it with the input form.
    
    * added the call for the five day forecast info. Again, need a submit button to test.
    
    * created an array with all chicago zip codes so we can varify user's zip code. Will now add zip codes for suburbs serverd by the L.
    
    * Updated zip code array with zips of suburbs directly  served by the CTA. Also added an if else statement that will check the users zip code input against that array. If it is in the array, we'll make calls for weather, otherwise we will prompt the user to enter a chicago zip code (the prompting code is to come.)
    
    * This commit is a test. Feels like after I push, my javascript file is marked as if I made a change. Gonna see if it does it.
    
    * Tested with a temporary form in index.html. If else works and the fiveday forecast. Still having trouble with the current weather api.
    
    * Got both weather apis working now. Leaving a text input form on index.html now for testing purposes.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,44 @@
-$(".button").on("click", function() {
+var chicagoMetroZips = ["60601","60602","60603","60604","60605","60606","60607","60608","60609","60610","60611","60612","60613","60614","60615","60616","60617","60618","60619","60620","60621","60622","60623","60624","60625","60626","60628","60629","60630","60631","60632","60633","60634","60636","60637","60638","60639","60640","60641","60642","60643","60644","60645","60646","60647","60649","60651","60652","60653","60654","60655","60656","60657","60659","60660","60661","60664","60666","60668","60669","60670","60673","60674","60675","60677","60678","60680","60681","60682","60684","60685","60686","60687","60688","60689","60690","60691","60693","60694","60695","60696","60697","60699","60701","60706","60707","60803","60804","60805","60827","60130","60201","60202","60203","60204","60208","60076","60077","60301","60302","60303","60304","60501","60804","60402","60546","60018","60019","60091"]
 
-    var queryURL =  "https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +$(this).attr("id") +"&outputType=JSON"
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
+$(document).ready(function(){
+
+  $(".button").on("click", function() {
+
+      var queryURL = "https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +$(this).attr("id") +"&outputType=JSON";
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      })
     })
-  })
 
+  // Note. We'll need to make the selector here match the html once it gets added, as well as the right thing where "#inputFormID" currently is.
+  $("#submitButton").on("click", function(event){
+    event.preventDefault();
+    var zipCode = $("#inputFormID").val().trim();
+    console.log(zipCode);
+    if(chicagoMetroZips.includes(zipCode)) {
+      console.log("I'm in the array")
+      var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&appid=4df0823f371a314fac922d45729a9495";
+      $.ajax({
+        url: weatherQueryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      })
+
+      var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us&appid=4df0823f371a314fac922d45729a9495";
+      $.ajax({
+        url: fiveDayQueryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      })
   
-
-
+    } else {
+      console.log("I'm not in the array.")
+      // Here we can put some sort of prompt asking the user to input a chicago zip
+    }
+  });
+});
