f9cab147330f199e061a109fb68de8b0e610fa08
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-23 14:18:07 -0500

    had to reorganize a bit so we don't make a rec before the api calls are complete.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
 var chicagoMetroZips = ["60601", "60602", "60603", "60604", "60605", "60606", "60607", "60608", "60609", "60610", "60611", "60612", "60613", "60614", "60615", "60616", "60617", "60618", "60619", "60620", "60621", "60622", "60623", "60624", "60625", "60626", "60628", "60629", "60630", "60631", "60632", "60633", "60634", "60636", "60637", "60638", "60639", "60640", "60641", "60642", "60643", "60644", "60645", "60646", "60647", "60649", "60651", "60652", "60653", "60654", "60655", "60656", "60657", "60659", "60660", "60661", "60664", "60666", "60668", "60669", "60670", "60673", "60674", "60675", "60677", "60678", "60680", "60681", "60682", "60684", "60685", "60686", "60687", "60688", "60689", "60690", "60691", "60693", "60694", "60695", "60696", "60697", "60699", "60701", "60706", "60707", "60803", "60804", "60805", "60827", "60130", "60201", "60202", "60203", "60204", "60208", "60076", "60077", "60301", "60302", "60303", "60304", "60501", "60804", "60402", "60546", "60018", "60019", "60091"]
 var badWeather = ["shower rain", "rain", "thunderstorm", "snow"];
+var commuteRec;
+var currentWeather;
+var futureWeather;
+var ctaInfo;
 
 $(document).ready(function () {
     
@@ -8,54 +12,48 @@ $(document).ready(function () {
 
 });
 
-
-$(".button").on("click", function () {
-      var queryURL = "https://cors-anywhere.herokuapp.com/https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +$(this).attr("id") +"&outputType=JSON";
-      $.ajax({
-
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    })
-})
-
-// Note. We'll need to make the selector here match the html once it gets added, as well as the right thing where "#inputFormID" currently is.
 $("#submitButton").on("click", function (event) {
     event.preventDefault();
-    var trainchoice = $("#trainmenu").val();
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +trainchoice +"&outputType=JSON";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(CTAresponse) {
-      var ctaInfo = CTAresponse;
-      console.log(ctaInfo);
-    })
     
     var zipCode = $("#inputFormID").val().trim();
     console.log(zipCode);
     if (chicagoMetroZips.includes(zipCode)) {
-        console.log("I'm in the array")
-        var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&appid=4df0823f371a314fac922d45729a9495";
+        var trainchoice = $("#trainmenu").val();
+        var queryURL = "https://cors-anywhere.herokuapp.com/https://www.transitchicago.com/api/1.0/routes.aspx?routeid=" +trainchoice +"&outputType=JSON";
         $.ajax({
-            url: weatherQueryURL,
-            method: "GET"
-        }).then(function (currentResponse) {
-            var currentWeather = currentResponse;
-            console.log(currentWeather.weather[0].description);
+          url: queryURL,
+          method: "GET"
+        }).then(function(CTAresponse) {
+          ctaInfo = CTAresponse;
+          console.log(ctaInfo);
+          var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&appid=4df0823f371a314fac922d45729a9495";
+            $.ajax({
+                url: weatherQueryURL,
+                method: "GET"
+            }).then(function (currentResponse) {
+                currentWeather = currentResponse;
+                console.log(currentWeather);
+                if (ctaInfo) {
+                    if ((badWeather.includes(currentWeather.weather[0].description)) || (currentWeather.main.temp > 273.15) || (ctaInfo.CTARoutes.RouteInfo.RouteStatus !== ("Planned Work" || "Normal Service"))) {
+                        commuteRec = "Stay Home";
+                    } else {
+                        commuteRec = "Enjoy your commute";
+                    } console.log(commuteRec);
+                }
+            })
+
+            var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us&appid=4df0823f371a314fac922d45729a9495";
+            $.ajax({
+                url: fiveDayQueryURL,
+                method: "GET"
+            }).then(function (futureResponse) {
+                futureWeather = futureResponse;
+                console.log(futureWeather);
+            })
         })
 
-        var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us&appid=4df0823f371a314fac922d45729a9495";
-        $.ajax({
-            url: fiveDayQueryURL,
-            method: "GET"
-        }).then(function (futureResponse) {
-            var futureWeather = futureResponse;
-            console.log(futureWeather);
-        })
+        
 
-        // Here, we're going to decide if it's a nice commute day today.
     } else {
         console.log("I'm not in the array.")
         // Here we can put some sort of prompt asking the user to input a chicago zip
