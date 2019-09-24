3ec75eb7a7a059f50ceb6147491adacfeaadea92
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-24 12:58:50 -0500

    Moved the blue line object out of app.js to its own file blueLineData.js Added a reference to that file to index.html. Tried it with a console log and it seems to work.

---
+++ assets/javascript/blueLineData.js
@@ -0,0 +1,134 @@
+var blueLineInfo = {
+    OHare: {
+        zip: 60666,
+        stationIDs: [30171.30172]
+    },
+    Rosemont: {
+        zip: 60018,
+        stationIDs: [30159,30160]
+    },
+    Cumberland: {
+        zip: 60631,
+        stationIDs: [30044,30045]
+    },
+    HarlemOHare: {
+        zip: 60656,
+        stationIDs: [30145,30146]
+    },
+    JeffersonPark: {
+        zip: 60630,
+        stationIDs: [30247,30248]
+    },
+    Montrose: {
+        zip: 60641,
+        stationIDs: [30259,30260]
+    },
+    IrvingPark: {
+        zip: 60613,
+        stationIDs: [30107,30108]
+    },
+    Addison: {
+        zip: 60618,
+        stationIDs: [30239,30240]
+    },
+    Belmont: {
+        zip: 60618,
+        stationIDs: [30012,30013]
+    },
+    LoganSquare: {
+        zip: 60647,
+        stationIDs: [30197,30198]
+    },
+    California: {
+        zip: 60647,
+        stationIDs: [30111,30112]
+    },
+    WesternOHare: {
+        zip: 60647,
+        stationIDs: [30129,30130]
+    },
+    Damen: {
+        zip: 60622,
+        stationIDs: [30115,30116]
+    },
+    Division: {
+        zip: 60622,
+        stationIDs: [30062,30063]
+    },
+    Chicago: {
+        zip: 60622,
+        stationIDs: [30271,30272]
+    },
+    Grand: {
+        zip: 60622,
+        stationIDs: [30095,30096]
+    },
+    ClarkLake: {
+        zip: 60601,
+        stationIDs: [30375,30374]
+    },
+    Washington: {
+        zip: 60602,
+        stationIDs: [30072,30073]
+    },
+    Monroe: {
+        zip: 60603,
+        stationIDs: [30153,30154]
+    },
+    Jackson: {
+        zip: 60604,
+        stationIDs: [30014,30015]
+    },
+    LaSalle: {
+        zip: 60605,
+        stationIDs: [30261,30262]
+    },
+    Clinton: {
+        zip: 60607,
+        stationIDs: [30084,30085]
+    },
+    UICHalsted: {
+        zip: 60607,
+        stationIDs: [30068,30069]
+    },
+    Racine: {
+        zip: 60607,
+        stationIDs: [30092,30093]
+    },
+    IllMedDistrict: {
+        zip: 60612,
+        stationIDs: [30157,30158]
+    },
+    WesternForestPark: {
+        zip: 60612,
+        stationIDs: [30042,30043]
+    },
+    KedzieHoman: {
+        zip: 60612,
+        stationIDs: [30048,30049]
+    },
+    Pulaski: {
+        zip: 60612,
+        stationIDs: [30179,30180]
+    },
+    Cicero: {
+        zip: 60644,
+        stationIDs: [30187,30188]
+    },
+    Austin: {
+        zip: 60304,
+        stationIDs: [30001,30002]
+    },
+    OakPark: {
+        zip: 60304,
+        stationIDs: [30034,30035]
+    },
+    HarlemForestPark: {
+        zip: 60130,
+        stationIDs: [30189,30190]
+    },
+    ForestPark: {
+        zip: 60130,
+        stationIDs: [30076,30077]
+    }
+}
