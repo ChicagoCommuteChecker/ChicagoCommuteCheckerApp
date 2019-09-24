232300932b2d7346f859c268f3cfa97a312187bc
Alojzy Klim <adklim@gmail.com>
2019-09-24 14:27:58 -0500

    added comma's in code

--- assets/javascript/blueLineData.js
+++ assets/javascript/blueLineData.js
@@ -1,165 +1,166 @@
 var blueLineInfo = {
     OHare: {
         zip: 60666,
-        stationIDs: [30171.30172]
+        stationIDs: [30171.30172],
         ada= "accessible"
     },
     Rosemont: {
         zip: 60018,
-        stationIDs: [30159,30160]
+        stationIDs: [30159,30160],
         ada= "accessible"
     },
     Cumberland: {
         zip: 60631,
-        stationIDs: [30044,30045]
+        stationIDs: [30044,30045],
         ada= "accessible"
     },
     HarlemOHare: {
         zip: 60656,
-        stationIDs: [30145,30146]
+        stationIDs: [30145,30146],
         ada= "accessible"
     },
     JeffersonPark: {
         zip: 60630,
-        stationIDs: [30247,30248]
+        stationIDs: [30247,30248],
         ada= "accessible"
     },
     Montrose: {
         zip: 60641,
-        stationIDs: [30259,30260]
+        stationIDs: [30259,30260],
         ada= "NOT accessible"
     },
     IrvingPark: {
         zip: 60613,
-        stationIDs: [30107,30108]
+        stationIDs: [30107,30108],
         ada= "NOT accessible"
     },
     Addison: {
         zip: 60618,
-        stationIDs: [30239,30240]
+        stationIDs: [30239,30240],
         ada= "accessible"
     },
     Belmont: {
         zip: 60618,
-        stationIDs: [30012,30013]
+        stationIDs: [30012,30013],
         ada= "NOT accessible"
     },
     LoganSquare: {
         zip: 60647,
-        stationIDs: [30197,30198]
+        stationIDs: [30197,30198],
         ada= "accessible"
     },
     California: {
         zip: 60647,
-        stationIDs: [30111,30112]
+        stationIDs: [30111,30112],
         ada= "NOT accessible"
     },
     WesternOHare: {
         zip: 60647,
-        stationIDs: [30129,30130]
+        stationIDs: [30129,30130],
         ada= "NOT accessible"
     },
     Damen: {
         zip: 60622,
-        stationIDs: [30115,30116]
+        stationIDs: [30115,30116],
         ada= "NOT accessible"
     },
     Division: {
         zip: 60622,
-        stationIDs: [30062,30063]
+        stationIDs: [30062,30063],
         ada= "NOT accessible"
     },
     Chicago: {
         zip: 60622,
-        stationIDs: [30271,30272]
+        stationIDs: [30271,30272],
         ada= "NOT accessible"
     },
     Grand: {
         zip: 60622,
-        stationIDs: [30095,30096]
+        stationIDs: [30095,30096],
         ada= "NOT accessible"
     },
     ClarkLake: {
         zip: 60601,
-        stationIDs: [30375,30374]
+        stationIDs: [30375,30374],
         ada= "accessible"
     },
     Washington: {
         zip: 60602,
-        stationIDs: [30072,30073]
+        stationIDs: [30072,30073],
     },
     Monroe: {
         zip: 60603,
-        stationIDs: [30153,30154]
+        stationIDs: [30153,30154],
         ada= "NOT accessible"
     },
     Jackson: {
         zip: 60604,
-        stationIDs: [30014,30015]
+        stationIDs: [30014,30015],
         ada= "accessible"
     },
     LaSalle: {
         zip: 60605,
-        stationIDs: [30261,30262]
+        stationIDs: [30261,30262],
         ada= "NOT accessible"
     },
     Clinton: {
         zip: 60607,
-        stationIDs: [30084,30085]
+        stationIDs: [30084,30085],
         ada= "NOT accessible"
     },
     UICHalsted: {
         zip: 60607,
-        stationIDs: [30068,30069]
+        stationIDs: [30068,30069],
         ada= "accessible"
     },
     Racine: {
         zip: 60607,
-        stationIDs: [30092,30093]
-        ada= " NOT accessible"
+        stationIDs: [30092,30093],
+        ada= "NOT accessible"
     },
     IllMedDistrict: {
         zip: 60612,
-        stationIDs: [30157,30158]
+        stationIDs: [30157,30158],
         ada= "accessible"
     },
     WesternForestPark: {
         zip: 60612,
-        stationIDs: [30042,30043]
+        stationIDs: [30042,30043],
         ada= "accessible"
     },
     KedzieHoman: {
         zip: 60612,
-        stationIDs: [30048,30049]
+        stationIDs: [30048,30049],
         ada= "accessible"
     },
     Pulaski: {
         zip: 60612,
-        stationIDs: [30179,30180]
+        stationIDs: [30179,30180],
         ada= "NOT accessible"
     },
     Cicero: {
         zip: 60644,
-        stationIDs: [30187,30188]
+        stationIDs: [30187,30188],
         ada= "NOT accessible"
     },
     Austin: {
         zip: 60304,
-        stationIDs: [30001,30002]
+        stationIDs: [30001,30002],
         ada= "NOT accessible"
     },
     OakPark: {
         zip: 60304,
-        stationIDs: [30034,30035]
+        stationIDs: [30034,30035],
         ada= "NOT accessible"
     },
     HarlemForestPark: {
         zip: 60130,
-        stationIDs: [30189,30190]
+        stationIDs: [30189,30190],
         ada= "NOT accessible"
     },
     ForestPark: {
         zip: 60130,
-        stationIDs: [30076,30077]
+        stationIDs: [30076,30077],
+        ada= "accessible"
     }
 }
