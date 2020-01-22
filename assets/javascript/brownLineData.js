7e1c81c08e2bbbe205d2c273e97688b78adc568c
Alojzy Klim <54486449+A-Klim01@users.noreply.github.com>
2020-01-21 18:44:08 -0600

    Update brownLineData.js

--- assets/javascript/brownLineData.js
+++ assets/javascript/brownLineData.js
@@ -2,37 +2,37 @@ var brownLineInfo = [
     {
         name: "Kimball",
         zip: "60625",
-        stationID: 40890,
+        stationID: 41290,
         ada: "accessible"
     },
     {
         name: "Kedzie",
         zip: "60025",
-        stationID: 40820,
+        stationID: 41180,
         ada: "accessible"
     },
     {
         name: "Francisco",
         zip: "60625",
-        stationID: 40230,
+        stationID: 40870,
         ada: "accessible"
     },
     {
         name: "Rockwell",
         zip: "60625",
-        stationID: 40750,
+        stationID: 41010,
         ada: "accessible"
     },
     {
         name: "Western",
         zip: "60625",
-        stationID: 41280,
+        stationID: 41480,
         ada: "accessible"
     },
     {
         name: "Damen",
         zip: "60625",
-        stationID: 41330,
+        stationID: 40090,
         ada: "accessible"
     },
     {
@@ -44,61 +44,61 @@ var brownLineInfo = [
     {
         name: "Irving Park",
         zip: "60613",
-        stationID: 41240,
+        stationID: 41460,
         ada: "accessible"
     },
     {
         name: "Addison",
         zip: "6061",
-        stationID: 40060,
+        stationID: 41440,
         ada: "accessible"
     },
     {
         name: "Paulina",
         zip: "60657",
-        stationID: 41020,
+        stationID: 41310,
         ada: "accessible"
     },
     {
         name: "Southport",
         zip: "60657",
-        stationID: 40570,
+        stationID: 40360,
         ada: "accessible"
     },
     {
         name: "Wellington",
         zip: "60657",
-        stationID: 40670,
+        stationID: 41210,
         ada: "accessible"
     },
     {
         name: "Diversey",
         zip: "60657",
-        stationID: 40590,
+        stationID: 40530,
         ada: "accessible"
     },
     {
         name: "Fullerton",
         zip: "60614",
-        stationID: 40320,
+        stationID: 41220,
         ada: "accessible"
     },
     {
         name: "Armitage",
         zip: "60614",
-        stationID: 41410,
+        stationID: 40660,
         ada: "accessible"
     },
     {
         name: "Sedgwick",
         zip: "60610",
-        stationID: 40490,
+        stationID: 40800,
         ada: "accessible"
     },
     {
         name: "Chicago",
         zip: "60610",
-        stationID: 40380,
+        stationID: 40710,
         ada: "accessible"
     },
     {
@@ -110,49 +110,49 @@ var brownLineInfo = [
     {
         name: "Washington Wells",
         zip: "60606",
-        stationIDs: 40790,
+        stationIDs: 40730,
         ada: "accessible"
     },
     {
         name: "Quincy",
         zip: "60606",
-        stationID: 40070,
+        stationID: 40040,
         ada: "accessible"
     },
     {
         name: "LaSalle Van Buren",
         zip: "60605",
-        stationID: 41340,
+        stationID: 40160,
         ada: "NOT accessible"
     },
     {
         name: "Harold Washington Library Van Buren",
         zip: "60604",
-        stationID: 40430,
+        stationID: 40850,
         ada: "accessible"
     },
     {
         name: "Adams Wabash",
         zip: "60603",
-        stationID: 40350,
+        stationID: 40680,
         ada: "NOT accessible"
     },
     {
         name: "Washington Wabash",
         zip: "60602",
-        stationID: 40470,
+        stationID: 41700,
         ada: "accessible"
     },
     {
         name: "State Lake",
         zip: "60601",
-        stationID: 40810,
+        stationID: 40260,
         ada: "NOT accessible"
     },
     {
         name: "Clark Lake",
         zip: "60601",
-        stationID: 40220,
+        stationID: 40380,
         ada: "accessible"
     },
 ];
