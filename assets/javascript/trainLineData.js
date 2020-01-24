25baa3bff72506781bc4f18f34f1430abbbabe9c
Alojzy Klim <adklim@gmail.com>
2020-01-24 12:50:10 -0600

    added Purpleline data to file

--- assets/javascript/trainLineData.js
+++ assets/javascript/trainLineData.js
@@ -848,3 +848,333 @@ var brownLineInfo = [
         ada: "accessible"
     },
 ];
+
+var brownLineInfo = [
+    {
+        name: "Kimball",
+        zip: "60625",
+        stationID: 41290,
+        ada: "accessible"
+    },
+    {
+        name: "Kedzie",
+        zip: "60025",
+        stationID: 41180,
+        ada: "accessible"
+    },
+    {
+        name: "Francisco",
+        zip: "60625",
+        stationID: 40870,
+        ada: "accessible"
+    },
+    {
+        name: "Rockwell",
+        zip: "60625",
+        stationID: 41010,
+        ada: "accessible"
+    },
+    {
+        name: "Western",
+        zip: "60625",
+        stationID: 41480,
+        ada: "accessible"
+    },
+    {
+        name: "Damen",
+        zip: "60625",
+        stationID: 40090,
+        ada: "accessible"
+    },
+    {
+        name: "Montrose",
+        zip: "60613",
+        stationID: 40550,
+        ada: "accessible"
+    },
+    {
+        name: "Irving Park",
+        zip: "60613",
+        stationID: 41460,
+        ada: "accessible"
+    },
+    {
+        name: "Addison",
+        zip: "6061",
+        stationID: 41440,
+        ada: "accessible"
+    },
+    {
+        name: "Paulina",
+        zip: "60657",
+        stationID: 41310,
+        ada: "accessible"
+    },
+    {
+        name: "Southport",
+        zip: "60657",
+        stationID: 40360,
+        ada: "accessible"
+    },
+    {
+        name: "Belmont",
+        zip: "60657",
+        stationID: 41320,
+        ada: "accessible"
+    },
+    {
+        name: "Wellington",
+        zip: "60657",
+        stationID: 41210,
+        ada: "accessible"
+    },
+    {
+        name: "Diversey",
+        zip: "60657",
+        stationID: 40530,
+        ada: "accessible"
+    },
+    {
+        name: "Fullerton",
+        zip: "60614",
+        stationID: 41220,
+        ada: "accessible"
+    },
+    {
+        name: "Armitage",
+        zip: "60614",
+        stationID: 40660,
+        ada: "accessible"
+    },
+    {
+        name: "Sedgwick",
+        zip: "60610",
+        stationID: 40800,
+        ada: "accessible"
+    },
+    {
+        name: "Chicago",
+        zip: "60610",
+        stationID: 40710,
+        ada: "accessible"
+    },
+    {
+        name: "Merchandise Mart",
+        zip: "60654",
+        stationID: 40370,
+        ada: "accessible"
+    },
+    {
+        name: "Washington/Wells",
+        zip: "60606",
+        stationIDs: 40730,
+        ada: "accessible"
+    },
+    {
+        name: "Quincy",
+        zip: "60606",
+        stationID: 40040,
+        ada: "accessible"
+    },
+    {
+        name: "LaSalle/Van Buren",
+        zip: "60605",
+        stationID: 40160,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Harold Washington Library-State/Van Buren",
+        zip: "60604",
+        stationID: 40850,
+        ada: "accessible"
+    },
+    {
+        name: "Adams/Wabash",
+        zip: "60603",
+        stationID: 40680,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Washington/Wabash",
+        zip: "60602",
+        stationID: 41700,
+        ada: "accessible"
+    },
+    {
+        name: "State/Lake",
+        zip: "60601",
+        stationID: 40260,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Clark/Lake",
+        zip: "60601",
+        stationID: 40380,
+        ada: "accessible"
+    },
+];
+
+var purpleLineInfo = [
+    {
+        name: "Linden",
+        zip: "60091",
+        stationID: 41050,
+        ada: "accessible"
+    },
+    {
+        name: "Central",
+        zip: "60201",
+        stationID: 41250,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Noyes",
+        zip: "60201",
+        stationID: 40400,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Foster",
+        zip: "60201",
+        stationID: 40520,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Davis",
+        zip: "60201",
+        stationID: 40050,
+        ada: "accessible"
+    },
+    {
+        name: "Dempster",
+        zip: "60201",
+        stationID: 40690,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Main",
+        zip: "60202",
+        stationID: 40270,
+        ada: "NOT accessible"
+    },
+    {
+        name: "South Blvd",
+        zip: "60202",
+        stationID: 40840,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Howard",
+        zip: "60626",
+        stationID: 40900,
+        ada: "accessible"
+    },
+    {
+        name: "Wilson",
+        zip: "60640",
+        stationID: 40540,
+        ada: "accessible"
+    },
+    {
+        name: "Belmont",
+        zip: "60657",
+        stationID: 41320,
+        ada: "accessible"
+    },
+    {
+        name: "Wellington",
+        zip: "60657",
+        stationID: 41210,
+        ada: "accessible"
+    },
+    {
+        name: "Wellington",
+        zip: "60657",
+        stationID: 41210,
+        ada: "accessible"
+    },
+    {
+        name: "Diversey",
+        zip: "60657",
+        stationID: 40530,
+        ada: "accessible"
+    },
+    {
+        name: "Fullerton",
+        zip: "60614",
+        stationID: 41220,
+        ada: "accessible"
+    },
+    {
+        name: "Armitage",
+        zip: "60614",
+        stationID: 40660,
+        ada: "accessible"
+    },
+    {
+        name: "Sedgwick",
+        zip: "60610",
+        stationID: 40800,
+        ada: "accessible"
+    },
+    {
+        name: "Chicago",
+        zip: "60610",
+        stationID: 41450,
+        ada: "accessible"
+    },
+    {
+        name: "Merchandise Mart",
+        zip: "60654",
+        stationID: 40460,
+        ada: "accessible"
+    },
+    {
+        name: "Clark/Lake",
+        zip: "60601",
+        stationIDs: 40380,
+        ada: "accessible"
+    },
+    {
+        name: "State/Lake",
+        zip: "60601",
+        stationID: 40260,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Washington/Wabash",
+        zip: "60602",
+        stationID: 41700,
+        ada: "accessible"
+    },
+    {
+        name: "Adams/Wabash",
+        zip: "60603",
+        stationID: 40680,
+        ada: "NOT accessible"
+    },
+    {
+        name: "Harold Washington Library-State/Van Buren",
+        zip: "60604",
+        stationID: 40850,
+        ada: "accessible"
+    },
+    {
+        name: "LaSalle/Van Buren",
+        zip: "60605",
+        stationID: 40160,
+        ada: "accessible"
+    },
+    {
+        name: "Quincy",
+        zip: "60606",
+        stationID: 40040,
+        ada: "accessible"
+    },
+    {
+        name: "Washington/Wells",
+        zip: "60606",
+        stationID: 40730,
+        ada: "accessible"
+    },
+];
